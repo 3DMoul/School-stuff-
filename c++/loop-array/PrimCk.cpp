5802f79ea6dfd32ccaee05d150f6c7465284fc06
3DMoul <svanteman123@yahoo.se>
2025-10-02 14:31:33 +0200

    Update PrimCk.cpp

--- c++/loop-array/PrimCk.cpp
+++ c++/loop-array/PrimCk.cpp
@@ -16,7 +16,7 @@ int PrimCk()
 		
 		std::cout << "skriv numb";
 		std::cin >> checknumb;
-		if (checknumb <= 1)
+		if (checknumb <= 1)// to see if its 1 or les so not prime number
 		{
 			fail = true;
 			run = false;
@@ -24,13 +24,13 @@ int PrimCk()
 
 		for (int i = 2; i < checknumb; i++)
 		{
-			if (checknumb % i == 0)
+			if (checknumb % i == 0)//to see if the number can cleanly divide with every number under it if it can its not a prime 
 			{
 				fail = true;
 				run = false;
 			}
 		}
-		run = false;
+		run = false;//this so its not infinit loop
 	}
 	if (fail == true)
 	{
@@ -46,4 +46,5 @@ int PrimCk()
 
 
 		return 0;
+
 }
