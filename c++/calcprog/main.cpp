91a169488cca9276405788dedc6b4a3c703bf298
3DMoul <svanteman123@yahoo.se>
2025-10-02 10:37:02 +0200

    Update and rename c++/main.cpp to c++/calcprog/main.cpp

--- c++/calcprog/main.cpp
+++ c++/calcprog/main.cpp
@@ -25,7 +25,7 @@ int main()
 		
 		while (!(std::cin >> Choice))
 		{
-			std::cout << "Fel du måste inputa ett nummer:  ";
+			std::cout << "Fel du mÃ¥ste inputa ett nummer:  ";
 			std::cin.clear();
 			std::cin.ignore(10000, '\n');
 		}
