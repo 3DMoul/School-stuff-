7c5b8808bb7896920bc04d7e62ca1f6210f5c25c
Svante <svanteman123@yahoo.se>
2025-10-23 09:23:01 +0200

    Refactor output messages to include unit 'C'

--- c++/inlämning 2. Class-Struct/Main2.cpp
+++ c++/inlämning 2. Class-Struct/Main2.cpp
@@ -11,23 +11,19 @@
 
 
 using namespace std;
-struct Measurement {
-    string timestamp;
-    float value;
-};
-
 
 int main()
 {
     std::list<std::vector<std::string>> DataEntries = {};
     ifstream TxTData;
-     
+     //går igenom värdena som var innan och skickar in dem i listan
     TxTData.open("DataVals.txt", ios::in);
     if (TxTData.is_open())
     {
         int times = 0;
         std::vector<std::string> Values(3);
         string line;
+        //här går den igenom alla lines av txt filen samt lägger dem in i vector som sätts in i listan
         while (getline(TxTData, line))
         {
 
@@ -85,19 +81,15 @@ int main()
             system("CLS");
 
             std::string TimesChoice = "How many values do you want to add";
-
+            //här inisierar jag hur många värden jag vill ha
             int Times = main.NumberChoice(TimesChoice);
-
 
             for (int i = 0; i < Times; i++)
             {
 
                 double InputVal = 0;
-                std::cin >> InputVal;
+                std::cin >> InputVal;//här kan man manuelt skriva vilket värde man vill ha
                 DataEntries.push_back(DataIn.DataListInput(i, DataEntries, InputVal));
-
-
-
 
             }
 
@@ -194,7 +186,7 @@ int main()
                 double SumVal = DataIn.SummOfList(DataEntries);
 
 
-                std::cout << "The only value is " << SumVal << std::endl;
+                std::cout << "The only value is " << SumVal << " C" << std::endl;
 
 
             }
@@ -208,33 +200,38 @@ int main()
 
                 //function för att summrera all data
                 double SumVal = DataIn.SummOfList(DataEntries);
-                std::cout << "The sum of all the values is " << SumVal << std::endl;
+                std::cout << "The sum of all the values is " << SumVal << " C" << std::endl;
 
 
                 // medelvärdet är summan av alla delat på hur många values du har
-                std::cout << "Avarage of all values " << SumVal / size(DataEntries) << std::endl;
+                std::cout << "Avarage of all values " << SumVal / size(DataEntries) << " C" << std::endl;
 
-
+                // här deklarerar jag både string float variablen för min och max värdet
                 std::string TimeMin, TimeMax, IdMin, IdMax;
                 float MinVal, MaxVal;
+
+                //här får jag ut värdet för både string och float värdet för minsta tal i listan
                 tie(TimeMin, IdMin, MinVal) = DataIn.PrintListMin(DataEntries);
-                std::cout << "Min: \n" << TimeMin << IdMin << "\n" << MinVal << "\n" << std::endl;
+                std::cout << "Min: \n" << TimeMin << IdMin << "\n" << MinVal << " C" << "\n" << std::endl;
 
+                //-||- för högsta tal i listan
                 tie(TimeMax, IdMax, MaxVal) = DataIn.PrintListMax(DataEntries);
-                std::cout << "Max: \n" << TimeMax << IdMax << "\n" << MaxVal << "\n" << std::endl;
+                std::cout << "Max: \n" << TimeMax << IdMax << "\n" << MaxVal << " C" << "\n" << std::endl;
 
-
+                //detta får jag värdet för variancen
                 double Varians = DataIn.Variance(DataEntries);
-                std::cout << "The sample varians is " << Varians / (size(DataEntries) - 1) << std::endl;
-                std::cout << "The population varians is " << Varians / size(DataEntries) << std::endl;
+                std::cout << "The sample varians is " << Varians / (size(DataEntries) - 1) << " C" << std::endl;//både normal
+                std::cout << "The population varians is " << Varians / size(DataEntries) << " C" << std::endl;//och population
 
+                
 
-                int TimesOver = 0;
-                int TimesUnder = 0;
-
-                if ((char)toupper(Des) == 'Y')
+                if ((char)toupper(Des) == 'Y')//kollar om jag har eneblat denna funktion
                 {
-
+                    //deklarering för valuelimit
+                    int TimesOver = 0;
+                    int TimesUnder = 0;
+                    
+                    // får båda values både för över samt under
                     tie(TimesOver, TimesUnder) = DataIn.ValueLimit(DataEntries, LimitValue);
 
                     std::cout << "this is how many times you were over the limit value [" << TimesOver << "]" << std::endl;
@@ -253,11 +250,9 @@ int main()
 
                     //har detta ifall man sätter EndBoundryn innnan BeginBoundryn för då kommer det inte funka
                     double MovingAvarageSum = DataIn.MovingAvarage(EndBoundry, BeginBoundry, DataEntries);
-                    std::cout << "Moving avarage is : " << MovingAvarageSum << std::endl;
+                    std::cout << "Moving avarage is : " << MovingAvarageSum << " C" << std::endl;
 
                 }
-
-
 
             }
 
@@ -278,16 +273,17 @@ int main()
 
         case 4:
         {
-
+            //simulering av random mätvärden
             system("CLS");
 
             std::string TimesSimulate = "How many values do you want to simulate";
 
+            //här inisierar jag hur många värden jag vill ha
             int Times = main.NumberChoice(TimesSimulate);
 
             for (int i = 0; i < Times; i++)
             {
-
+                //detta tar fram ett random decimaltal mellan 20-30
                 double f = (double)rand() / RAND_MAX;
                 double RandValue = 20 + f * (30 - 20);
                 DataEntries.push_back(DataIn.DataListInput(i, DataEntries, RandValue));
